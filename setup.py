27138a634936ba947cec75248ced6d462afcb7ab
Jonas Wagner <jwagner@raptus.com>
2012-11-02 06:58:12 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0b3'
 setup(name='pcommerce.stock',
       version=version,
       description="Provides stock functionality for PCommerce",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -16,7 +16,7 @@ setup(name='pcommerce.stock',
       keywords='',
       author='Raptus AG',
       author_email='dev@raptus.com',
-      url='https://svn.plone.org/svn/collective/pcommerce.stock',
+      url='https://github.com/Raptus/pcommerce.stock',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['pcommerce'],
